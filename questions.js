681427d64f84fcf7bf1713d9cc398baf1cc1e51b
Jamie Perrier <67609017+BlueJay1086@users.noreply.github.com>
2020-08-06 20:11:00 -0400

    Add files via upload
    initial upload of assets

---
+++ questions.js
@@ -0,0 +1,81 @@
+var questions =[{
+    question: 'Commonly known as the "Barkless Dog", this small breed from Africa has a distinctive "yodel" that it uses to comunicate.',
+	 answers: ['Beagle'
+	 ,'Sloughi'
+	 ,'Basenji'
+     ,'Maltese'],
+     correctAnswer : 2
+},
+{
+    question :'This arctic working dog is one of the most common breeds to be crossed with a wolf.',
+	answers:[' Siberian Husky'
+	,' Bernese Mountain Dog'
+	,' Lhasa Apso'
+    ,' Samoyed'],
+    correctAnswer: 0
+},
+{
+    question :'These water retrievers are popular parents for many different "designer" dogs.',
+	answers:[' German Shephard'
+	,' Portugese Water Dog'
+    ,' Boxer'
+    ,' Standard Poodle'],
+    correctAnswer: 3
+},
+{
+    question :'This distinctive-looking dog was bred to guard horses and coaches, and now rides in big red trucks.',
+	answers:[' Great Dane'
+	,' Dalmation'
+    ,' German Shepherd'
+    ,' Cocker Spaniel'],
+    correctAnswer: 1
+},
+{
+    question :'This small, Japanese hunting breed is often called "catlike" for its tendency to bond strongly to a single person and ignore everyone else.',
+	answers:[' Shiba Inu'
+	,' Beagle'
+    ,' Pointer'
+    ,' Akita'],
+    correctAnswer: 0
+},
+{
+    question :'These hunting breeds get their name from the ability to freeze in place and look straight at its prey to tell the hunter where it is hiding.',
+	answers:[' Beagles'
+	,' Setters'
+    ,' Shepherds'
+    ,' Pointers'],
+    correctAnswer: 3
+},
+{
+    question :'Huskies are not the only dogs bred to move loads. These drafting dogs were bred to pull carts of hay around the farm.',
+	answers:[' Burnese Mountain Dog'
+	,' Giant Schnauzer'
+    ,' Leonburger'
+    ,' Mastiff'],
+    correctAnswer: 0
+},
+{
+    question :'This diminuituve breed comes in two varieties, the striking "Hairless" and the cuddly "powderpuff".',
+	answers:[' Havanese'
+	,' Chinese Crested'
+    ,' Yorkshire Terrier'
+    ,' Maltese'],
+    correctAnswer: 1
+},
+{
+    question :'This beloved breed came to America from England where it was used as a fighting/baiting dog, and is a common ancestor to many of today\'s "bully" and "pit" breeds.',
+	answers:[' Staffordshore Terrier'
+	,' Bulldog'
+    ,' Pit Bull'
+    ,' Bull Terrier'],
+    correctAnswer: 1
+},
+{
+    question :'This comically short dog is an expert at herding and a favorite of English royalty.',
+	answers:[' Border Collie'
+	,' Anatolian Shepherd Dog'
+    ,' Australian Sheepdog'
+    ,' Corgi'],
+    correctAnswer: 3
+}]
+
---
+++ script.js
@@ -0,0 +1,60 @@
+var start = document.querySelector("#start")
+var homepage = document.querySelector("#homePage")
+var quizPage = document.querySelector("#quizPage")
+var timerEl = document.querySelector("#time")
+var currentQuestion = 0
+var secondsLeft = 30
+var theTimer
+
+function timer (){
+    theTimer = setInterval(function(){
+        secondsLeft--
+        timerEl.textContent = secondsLeft
+        if(!secondsLeft){
+            clearInterval(theTimer)
+            console.log('end game')
+        }
+    },1000)
+}
+
+function displayQuestion() {
+    var questionEl = document.querySelector("#question")
+    questionEl.textContent = questions[currentQuestion].question
+    var optionsEl = document.querySelector("#options")
+    optionsEl.innerHTML =''
+    for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
+        var button = document.createElement("button")
+        button.textContent = questions[currentQuestion].answers[i]
+        button.setAttribute("value", i)
+        button.setAttribute("class", "answer")
+        optionsEl.appendChild(button)
+    }
+}
+
+function init(){
+    homepage.setAttribute('class','d-none')
+    quizPage.classList.remove("d-none")
+    timer()
+    console.log("clicked");
+    displayQuestion()
+}
+
+function checkAnswer(){
+    if(!event.target.classList.contains('answer')) return
+    console.log(event.target.value, questions[currentQuestion].correctAnswer )
+    if(questions[currentQuestion].correctAnswer !== parseInt(event.target.value)){
+        secondsLeft -= 15
+    }
+    currentQuestion++
+    console.log(currentQuestion ,questions.length)
+    if(currentQuestion === questions.length){
+        console.log('end game')
+    }else{
+        displayQuestion()
+    }
+}
+
+
+document.addEventListener('click', checkAnswer)
+
+start.addEventListener("click", init)
